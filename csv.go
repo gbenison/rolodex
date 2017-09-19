722b7c0cef3e69c4d1ec21f34005a72bd413281f
Greg Benison <gbenison@gmail.com>
2017-09-19 07:21:52 -0700

    Minimal working CSV import

---
+++ csv.go
@@ -0,0 +1,39 @@
+package rolodex
+
+import (
+       "log"
+       "os"
+       "io"
+       "encoding/csv"
+       "strconv"
+)
+
+type AddressRecord struct {
+     FirstName, LastName, StreetAddress, City, State string
+     Zipcode int
+}
+
+func ReadAddressCSV(fname string, c chan AddressRecord) {
+     f, err := os.Open(fname)
+     if err != nil {
+     	log.Fatal(err)
+     }
+     reader := csv.NewReader(f)
+     reader.Comment = '#'
+     for {
+         record, err := reader.Read()
+	 if err == io.EOF {
+	    break
+	 }
+	 if err != nil {
+	    log.Fatal(err)
+	 }
+         zipcode, err := strconv.Atoi(record[5])
+         if err != nil {
+             log.Print(err)
+         } else {
+             address_record := AddressRecord{record[0], record[1], record[2], record[3], record[4], zipcode}
+             c <- address_record
+	 }
+     }
+}
---
+++ csv_test.go
@@ -0,0 +1,34 @@
+package rolodex
+
+import (
+       "testing"
+       "os"
+)
+
+func TestReadFile(t *testing.T) {
+     _, err := os.Open("testdata/enemies.csv")
+     if err != nil {
+     	t.Errorf("Could not open test data")
+     }
+}
+
+func TestRecord(t *testing.T) {
+     c := make(chan AddressRecord)
+     go ReadAddressCSV("testdata/enemies.csv", c)
+
+     first_record := <- c
+     if first_record.FirstName != "Greg" {
+     	t.Errorf("Expected FirstName Greg; got %s", first_record.FirstName)
+     }
+     if first_record.StreetAddress != "404 N. Found St." {
+     	t.Errorf("Expected StreetAddress 404 N. Found St.; got %s", first_record.StreetAddress)
+     }
+     if first_record.Zipcode != 97221 {
+     	t.Errorf("Expected zip code 97221; got %d", first_record.Zipcode)
+     }
+
+     second_valid_record := <- c
+     if second_valid_record.FirstName != "Swiper" {
+     	t.Errorf("Expected FirstName Swiper; got %s", second_valid_record.FirstName)
+     }
+}
